a9f090c764fe4a7565f8f3dc28733a91a0cf06d8
shamilsdq <shamilsdq@live.com>
2022-10-18 22:32:57 +0530

    Setup route for rendering React app in rails

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     post 'seating' => 'seating#generate', as: 'generate_seating'
   end
+
+  root 'home#index'
+  get '*path', to: 'home#index', via: :all
 end
