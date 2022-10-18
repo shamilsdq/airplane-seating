9f3b860afd6dc67ba51f599828480246de8cdf01
shamilsdq <shamilsdq@live.com>
2022-10-18 22:12:12 +0530

    Updated controller to accept params and return response

--- app/controllers/api/seating_controller.rb
+++ app/controllers/api/seating_controller.rb
@@ -1,6 +1,20 @@
 class Api::SeatingController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def generate
-    # generate seating from passed params
-    render json: {}
+    response = generate_seating
+    render json: response
+  end
+
+  private
+
+  def generate_seating
+    SeatingService
+      .new(seating_params[:dimensions])
+      .get_seating_arrangement(seating_params[:count])
+  end
+
+  def seating_params
+    params.require(:seating).permit(:count, dimensions: %i[rows columns])
   end
 end
